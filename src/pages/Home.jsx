ead777ea2b3f56ed3b975f85934fa5c9fcc126f9
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-06-05 22:04:12 +0530

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+
+function Home() {
+    const API_URL = "https://fakestoreapi.com/products";
+    return (
+        <>
+        <div>Home</div>
+        </>
+    )
+}
+
+export default Home

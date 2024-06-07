21344c6a0660b34def99187ab3a7d60802907317
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-06-07 17:41:51 +0530

    Added the Spinner Component For loading State

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Spinner() {
+    return (
+        <div>
+            <div className="flex items-center justify-center h-screen">
+                <div className="spinner"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner

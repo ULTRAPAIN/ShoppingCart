a8266c246bcb05cb7166c815035ac2917b908751
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-06-09 15:33:52 +0530

    changes

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,10 +1,10 @@
-// now lets create the store
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./Slices/CartSlice";
 
 export const store = configureStore({
-    reducer:{
-        cart:cartReducer, // cart is the name of that slice, whereas CartSlice is the name of that file .
-    }
-})
+    reducer: {
+        cart: cartReducer,
+    },
+});
 
+export default store;

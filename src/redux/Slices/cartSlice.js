5ebde031385df627ec25e6f2e09caf5d49bf52fb
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-06-09 13:25:31 +0530

    changes in syntax

--- src/redux/Slices/cartSlice.js
+++ src/redux/Slices/cartSlice.js
@@ -1,19 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// so lets first create the slice 
-
 export const cartSlice = createSlice({
-    name:"cart",
-    initialState:[],
-    reducers:{
-        add:(state,action)=>{
-            state.push(action.payload)
+    name: "cart",
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push(action.payload);
         },
-        remove:(state,action)=>{
-            return state.filter((item)=>item.id!==action.payload)
+        remove: (state, action) => {
+            return state.filter((item) => item.id !== action.payload);
         },
-    }
-})
+    },
+});
 
-export const {add,remove} = cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
 export default cartSlice.reducer;
